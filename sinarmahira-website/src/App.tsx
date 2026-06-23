8ddae4433284ccafdb9f20125d20268981bfdf2e
Rastyaa <agung.laksana990@gmail.com>
2026-06-24 01:57:38 +0700

    feat: add Vercel Analytics to the application for tracking user insights

--- sinarmahira-website/src/App.tsx
+++ sinarmahira-website/src/App.tsx
@@ -1,8 +1,12 @@
 import AppRoutes from './routes/AppRoutes';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
-    <AppRoutes />
+    <>
+      <AppRoutes />
+      <Analytics />
+    </>
   );
 }
 
